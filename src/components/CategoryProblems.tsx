036454dcff982390cc91b123654c936ad8ab831d
hoyyChoi <hoyeon8165@gmail.com>
2025-08-19 02:40:24 +0900

    chore: 1r124

--- src/components/CategoryProblems.tsx
+++ src/components/CategoryProblems.tsx
@@ -19,7 +19,7 @@ const CategoryProblems = ({
       name: "React",
       icon: "⚛️",
       description:
-        "Re111actㄱㄱㄱㄴ 프레임111워1111111ㅗㅗㅗ크eee 11111관련 문제들",
+        "Re111actㄱㄱㄱㄴ 프레임111워1111111ㅗㅗㅗ크eee 11111관련 문제들123213412",
       problems: [
         {
           id: "react-001",
